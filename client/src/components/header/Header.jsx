680bf849f4969ecc469f2a1d5be27153e35e1992
npvinh04 <nqvinh1995@gmail.com>
2022-09-18 23:10:45 +0700

    update cart

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Search from "../search/Search";
+import logo  from "../../image/logo-full-white.png";
 import "./header.css";
 import Navbar from "../navbar/Navbar";
+import Cart from "../cart/Cart";
 
 const Header = () => {
     return (
         <div className="shopee-top shopee-top--sticky">
-            <Navbar/>
-            <Search/>
+            <div className="navbar-wrapper">
+                <Navbar/>
+            </div>
+            <div className="header-with-search-wrapper">
+                <div className="logo">
+                    <img src={logo} alt='logo'/>
+                </div>
+                <Search/>
+                <Cart/>
+            </div>
+
         </div>
     )
 };
