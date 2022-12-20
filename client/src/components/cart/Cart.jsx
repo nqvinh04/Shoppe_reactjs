33c9ad019f7cfd55375579d9a4e9065b5fb9ee5b
npvinh04 <nqvinh1995@gmail.com>
2022-12-20 23:33:55 +0700

    update css for component Cart

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import LogoCart from '../../image/shopping-cart.png';
 import { Link } from "react-router-dom";
+import "./cart.css";
 
 
 const Cart = () => {
     return (
         <div className="container-with-search__cart-wrapper">
             <div className="stardust-popover" id="cart_drawer_target_id">
-                <div role="button" className="stardust-popover_target">
+                <div role="button" className="stardust-popover__target">
                     <div className="cart-drawer-container">
                         <Link to="/cart" className="cart-drawer">
                             <img src={LogoCart}
