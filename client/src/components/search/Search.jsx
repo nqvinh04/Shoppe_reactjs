a6509e4c7d1a8bd2192228017634b56b81c719db
npvinh04 <nqvinh1995@gmail.com>
2022-12-21 00:08:34 +0700

    update css for component search

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './search.css';
 
 const Search = () => {
 
@@ -7,32 +8,29 @@ const Search = () => {
     }
 
     return (
-        <>
-            <div className="header-with-search_search-section">
-                <div className="shopee-searchbar">
-                    <div className="shopee-searchbar__main">
-                        <form >
-                            <input type="text" placeholder="Sale Xả Hàng"/>
-                        </form>
-                    </div>
-                    <button
-                        type="button"
-                        onClick={() => handleSearch()}
-                    />
+        <div className="header-with-search_search-section">
+            <div className="shopee-searchbar">
+                <div className="shopee-searchbar__main">
+                    <form >
+                        <input type="text" placeholder="Sale Xả Hàng"/>
+                    </form>
                 </div>
-                <div className="FaT0DB">
-                    <a className="UPUwyq" href="/search?keyword=%C3%A1o%20kho%C3%A1c">Áo Khoác</a>
-                    <a className="UPUwyq" href="/search?keyword=%C3%A1o%20len">Áo Len</a>
-                    <a className="UPUwyq" href="/search?keyword=d%C3%A9p">Dép</a>
-                    <a className="UPUwyq" href="/search?keyword=%C3%A1o%20kho%C3%A1c%20nam">Áo Khoác Nam</a>
-                    <a className="UPUwyq" href="/search?keyword=t%C3%BAi%20x%C3%A1ch%20n%E1%BB%AF">Túi Xách Nữ</a>
-                    <a className="UPUwyq" href="/search?keyword=gi%C3%A0y%20nam">Giày Nam</a>
-                    <a className="UPUwyq" href="/search?keyword=%C3%A1o%20croptop">Áo Croptop</a>
-                    <a className="UPUwyq" href="/search?keyword=lego">LEGO</a>
-                </div>
+                <button
+                    type="button"
+                    onClick={() => handleSearch()}
+                />
             </div>
-        </>
-
+            <div className="FaT0DB">
+                <a className="UPUwyq" href="/search?keyword=%C3%A1o%20kho%C3%A1c">Áo Khoác</a>
+                <a className="UPUwyq" href="/search?keyword=%C3%A1o%20len">Áo Len</a>
+                <a className="UPUwyq" href="/search?keyword=d%C3%A9p">Dép</a>
+                <a className="UPUwyq" href="/search?keyword=%C3%A1o%20kho%C3%A1c%20nam">Áo Khoác Nam</a>
+                <a className="UPUwyq" href="/search?keyword=t%C3%BAi%20x%C3%A1ch%20n%E1%BB%AF">Túi Xách Nữ</a>
+                <a className="UPUwyq" href="/search?keyword=gi%C3%A0y%20nam">Giày Nam</a>
+                <a className="UPUwyq" href="/search?keyword=%C3%A1o%20croptop">Áo Croptop</a>
+                <a className="UPUwyq" href="/search?keyword=lego">LEGO</a>
+            </div>
+        </div>
     )
 }
 export default Search;
