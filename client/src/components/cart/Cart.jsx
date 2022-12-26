cd2ca882eda75cfee630edd30c93d77cc7ec73a6
npvinh04 <nqvinh1995@gmail.com>
2022-12-27 00:11:01 +0700

    edit css search

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LogoCart from '../../image/shopping-cart.png';
+// import LogoCart from '../../image/shopping-cart.png';
 import { Link } from "react-router-dom";
 import "./cart.css";
 
@@ -10,11 +10,19 @@ const Cart = () => {
             <div className="stardust-popover" id="cart_drawer_target_id">
                 <div role="button" className="stardust-popover__target">
                     <div className="cart-drawer-container">
-                        <Link to="/cart" className="cart-drawer">
-                            <img src={LogoCart}
-                                 alt='Cart'
-                                 className="shopee-png-icon navbar__link-icon icon-shop-card-2"
-                            />
+                        <Link to="/cart" className="cart-drawer flex v-center" id="cart_drawer_target_id">
+                            {/*<img src={LogoCart}*/}
+                            {/*     alt='Cart'*/}
+                            {/*     className="shopee-png-icon navbar__link-icon icon-shop-card-2"*/}
+                            {/*/>*/}
+                            <svg viewBox="0 0 26.6 25.6"
+                                 className="shopee-svg-icon navbar__link-icon icon-shopping-cart-2">
+                                <polyline fill="none" points="2 1.7 5.5 1.7 9.6 18.3 21.2 18.3 24.6 6.1 7 6.1"
+                                          strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10"
+                                          strokeWidth="2.5"></polyline>
+                                <circle cx="10.7" cy="23" r="2.2" stroke="none"></circle>
+                                <circle cx="19.7" cy="23" r="2.2" stroke="none"></circle>
+                            </svg>
                         </Link>
                     </div>
                 </div>
--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -55,14 +55,16 @@ const Search = () => {
                 </button>
             </div>
             <div className="FaT0DB">
-                <a className="UPUwyq" href="/search?keyword=%C3%A1o%20kho%C3%A1c">Áo Khoác</a>
-                <a className="UPUwyq" href="/search?keyword=%C3%A1o%20len">Áo Len</a>
-                <a className="UPUwyq" href="/search?keyword=d%C3%A9p">Dép</a>
-                <a className="UPUwyq" href="/search?keyword=%C3%A1o%20kho%C3%A1c%20nam">Áo Khoác Nam</a>
-                <a className="UPUwyq" href="/search?keyword=t%C3%BAi%20x%C3%A1ch%20n%E1%BB%AF">Túi Xách Nữ</a>
-                <a className="UPUwyq" href="/search?keyword=gi%C3%A0y%20nam">Giày Nam</a>
-                <a className="UPUwyq" href="/search?keyword=%C3%A1o%20croptop">Áo Croptop</a>
-                <a className="UPUwyq" href="/search?keyword=lego">LEGO</a>
+                <div className="pFceVI">
+                    <a className="UPUwyq" href="/search?keyword=%C3%A1o%20kho%C3%A1c">Áo Khoác</a>
+                    <a className="UPUwyq" href="/search?keyword=%C3%A1o%20len">Áo Len</a>
+                    <a className="UPUwyq" href="/search?keyword=d%C3%A9p">Dép</a>
+                    <a className="UPUwyq" href="/search?keyword=%C3%A1o%20kho%C3%A1c%20nam">Áo Khoác Nam</a>
+                    <a className="UPUwyq" href="/search?keyword=t%C3%BAi%20x%C3%A1ch%20n%E1%BB%AF">Túi Xách Nữ</a>
+                    <a className="UPUwyq" href="/search?keyword=gi%C3%A0y%20nam">Giày Nam</a>
+                    <a className="UPUwyq" href="/search?keyword=%C3%A1o%20croptop">Áo Croptop</a>
+                    <a className="UPUwyq" href="/search?keyword=lego">LEGO</a>
+                </div>
             </div>
         </div>
     )
